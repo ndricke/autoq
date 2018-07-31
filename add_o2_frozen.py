8fb775ddc80c0250647fe43e9ff5b52e0e74726c
jbiolchen <kellyjchen12@gmail.com>
2018-07-31 15:48:18 -0400

    Add files via upload
    scripts for randomly functionalizing porFe, binding O2

---
+++ add_o2_frozen.py
@@ -0,0 +1,70 @@
+# input: directory containing bare catalyst .mol and/or .xyz files
+# creates .xyz files for corresponding O2-bound structures
+# catalysts with one metal atom only
+
+import os, sys
+import molSimplify
+from molSimplify.Scripts import structgen
+from molSimplify.Classes import mol3D, atom3D
+import numpy as np
+import math
+
+def scale_vector(vec, new_mag):
+    squared = 0.0
+    new_mag = float(new_mag)
+    scale = new_mag / np.linalg.norm(vec)
+    return [c * scale for c in vec]
+
+def vec3_perp(vec): # janky orthogonal vector generator; 3D
+    if len(vec) != 3 or vec == [0, 0, 0]:
+        print("Not a suitable vector.")
+        return None
+    ihat = [1, 0, 0] # arbitrarily chosen
+    return scale_vector(np.cross(vec, ihat), np.linalg.norm(vec)) # retains original magnitude
+
+indir = sys.argv[1].rstrip('/')
+
+for infile in os.listdir(indir):
+    infile_type = infile.split('.')[-1]
+    if infile_type != "mol" and infile_type != "xyz":
+        continue
+    print(infile)
+
+    mol3D_O2 = mol3D.mol3D()
+    mol3D_O2.OBMol = mol3D_O2.getOBMol(indir + '/' + infile, infile_type, ffclean = False)
+    mol3D_O2.convert2mol3D()
+
+    if len(mol3D_O2.findMetal()) == 1:
+        M_ind = mol3D_O2.findMetal()[0]
+    else:
+        print("%s contains %d metal atoms." %(infile, len(mol3D_O2.findMetal())))
+        continue
+
+    connection_list = mol3D_O2.getBondedAtomsSmart(M_ind, oct = False)
+    #print(connection_list)
+
+    mol, enl = structgen.ffopt('MMFF94', mol3D_O2, connection_list, 1, [], False, [], 200, False)
+
+    # manually bind O2 at an angle (M-O-O = 1.8 A, 120 deg, 1.3 A)
+    try:
+        v1 = mol3D_O2.atoms[connection_list[0]].distancev(mol3D_O2.atoms[connection_list[1]])
+        v2 = mol3D_O2.atoms[connection_list[0]].distancev(mol3D_O2.atoms[connection_list[2]])
+        v = np.cross(v1, v2)
+    except:
+        print("Error finding plane of catalyst.")
+        continue
+
+    v_O1 = scale_vector(v, 1.8)
+    v_O2 = scale_vector(v, 1.8 + 1.3 * math.sin(math.pi / 6))
+    v_perp = vec3_perp(scale_vector(v, 1.3 * math.cos(math.pi / 6)))
+    if v_perp == None: # shouldn't happen
+        continue
+    M_coords = mol3D_O2.atoms[M_ind].coords()
+
+    mol3D_O2.addAtom(mol3D.atom3D(Sym = 'O', xyz = [M_coords[c] + v_O1[c] for c in range(len(M_coords))]))
+    mol3D_O2.addAtom(mol3D.atom3D(Sym = 'O', xyz = [M_coords[c] + v_O2[c] + v_perp[c] for c in range(len(M_coords))]))
+
+    #mol, enl = structgen.ffopt('UFF', mol3D_O2, connection_list, 1, [], False, [], 200, False)
+    # i think this ffopt always fails for some reason
+
+    mol3D_O2.writexyz(infile.split('.')[0] + "O2")
---
+++ functionalize_catalyst.py
@@ -0,0 +1,117 @@
+# contains functions for randomly functionalizing atom indices
+# ligand should be added to molSimplify library first
+# there are currently no checks to make sure that number of requested items
+    # does not exceed max possible number of items, so be careful (probably fine)
+
+import os, sys
+import molSimplify
+from molSimplify.Scripts import structgen
+from molSimplify.Classes import mol3D, atom3D
+import numpy as np
+import math, random
+
+# def find_Hs(infile):
+#     # infile: .mol or .xyz
+#     # returns list of indices for all Hs in molecule
+#     infile_type = infile.split('.')[-1]
+#     if not infile_type == ('mol' or 'xyz'):
+#         print("Couldn't read molecule from input file.")
+#         return None
+#     molecule = mol3D.mol3D()
+#     molecule.OBMol = molecule.getOBMol(infile, infile_type, ffclean = False)
+#     molecule.convert2mol3D()
+#
+#     H_list = []
+#     for i in range(len(molecule.atoms)):
+#         if atoms[i].symbol() == 'H':
+#             H_list.append(i)
+#     return H_list
+
+def find_Hs(macrocycle): # string
+    """ this is a hard-coded list for the porphyrin in molSimplify's library,
+    for testing purposes; i'll go back later and figure out how to use any .mol
+    file from one's local library for the commented-out version of find_Hs() """
+    if macrocycle == "porphyrin":
+        return [9, 10, 17, 18, 24, 25, 30, 31, 33, 34, 35, 36] #one-indexed from their porphyrin.mol
+    else:
+        print("Not a valid ligand.")
+        return None
+
+def choose_items(list, num_items, no_repeats):
+    chosen = [] # contains objects, not indices
+    for n in range(num_items):
+        index = random.randrange(0, len(list))
+        if no_repeats: # list items should all be unique
+            while list[index] in chosen:
+                index = random.randrange(0, len(list))
+        chosen.append(list[index])
+    return chosen
+
+def make_tempdir_outdir(macrocycle, core):
+    tempdir = "/home/kjchen/functionalizecatalysttempdir" # modify as needed
+    while os.path.exists(tempdir):
+        tempdir += "0"
+    os.makedirs(tempdir)
+
+    outdir = "/home/kjchen/%s%s_funcs" %(macrocycle, core) # modify as needed; molSimplify expands ~ to home directory
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    else:
+        version = 2
+        outdir += str(version)
+        while os.path.exists(outdir):
+            version += 1
+            outdir = "/home/kjchen/%s%s_funcs%d" %(macrocycle, core, version)
+        os.makedirs(outdir)
+
+    return tempdir, outdir
+
+def list_to_string(list): # no spaces
+    rtn = "["
+    for item in list:
+        rtn += str(item) + ","
+    rtn = rtn[:-1] + "]"
+    return rtn
+
+def functionalize(macrocycle, core, possible_func_indices, expected_num_funcs, num_molecules):
+    # func_library: SMILES strings or built-in ligands; modify as needed
+    # generally there are issues reading in strings containing []()
+    func_library = ["B", "C", "N", "O", "F", "P", "S", "Cl", "Br", "I",
+                    "cyanide", "NC", "nme3", "tricyanomethyl", "methylamine",
+                    "dicyanamide", "imine", "nitroso", "misc",
+                    "OC", "oxo", "carboxyl",
+                    "trifluoromethyl",
+                    "thiocyanate",
+                    "benzene_pi", "pyridine", "phosphorine", "benzenethiol"]
+    for n in range(1, num_molecules + 1):
+        file_name = macrocycle + core + "-functionalized" + str(n)
+        num_funcs = 0
+        while (num_funcs < 1 or num_funcs > len(possible_func_indices)):
+            num_funcs = np.random.poisson(expected_num_funcs)
+        func_list = choose_items(func_library, num_funcs, False)
+        func_indices = choose_items(possible_func_indices, num_funcs, True)
+
+        ms_command = "molsimplify -core %s -oxstate 2 -coord 6 -geometry oct -spin 3 -lig %s -ligocc 1 -decoration %s -decoration_index %s -rundir %s -name %s" %(core, macrocycle, list_to_string(func_list), list_to_string(func_indices), tempdir, file_name)
+        print(ms_command)
+        os.system(ms_command)
+
+def collect_xyz_files(current_location, destination, delete_current_location):
+    # janky workaround for molSimplify's directory structure
+    # copies all .xyz files in current_location to destination
+    # does not check for duplicate molecules or file names
+    # should this skip "badjob" files? don't expect those to happen but just in case
+    for subdir, dirs, files in os.walk(current_location):
+        for file in files:
+            if file.split('.')[-1] == "xyz":
+                os.system("scp %s %s" %(os.path.join(subdir, file), destination))
+    if delete_current_location:
+        os.system("rm -R %s" %current_location)
+        # it also leaves a CLIinput.inp behind
+
+
+macrocycle, core, possible_func_indices, expected_num_funcs, num_molecules = "porphyrin", "Fe", find_Hs("porphyrin"), int(sys.argv[1]), int(sys.argv[2]) # testing
+tempdir, outdir = make_tempdir_outdir(macrocycle, core)
+
+functionalize(macrocycle, core, possible_func_indices, expected_num_funcs, num_molecules)
+print("Functionalized .xyz files placed in " + outdir)
+collect_xyz_files(tempdir, outdir, True)
