4d34866ba74809877d4ba29d3730205b7d86d6c5
Kelly Chen <kellyjchen12@gmail.com>
2018-08-23 14:01:05 -0400

    bug workaround: take bad optcdftsp.out, create cdftsp.in with optimized geo

--- ChemData.py
+++ ChemData.py
@@ -33,7 +33,7 @@ def lines2arr(line_list):
     for i, line in enumerate(line_list):
         mat[i,:] = line2floats(line)
     return mat
-        
+
 def KeyGrab(key_end, infile, start_cut=0, end_cut=0):
     grab_list = []
     [next(infile) for i in range(start_cut)] #sometimes we want to skip past a key_end
@@ -47,7 +47,7 @@ def KeyGrab(key_end, infile, start_cut=0, end_cut=0):
         return grab_list
     else:
         return grab_list[:(-1*end_cut)]
-        
+
 
 def sectionGrab(key_start,key_end,infile,start_cut=0,end_cut=0):
   y_read = 0
@@ -61,7 +61,7 @@ def sectionGrab(key_start,key_end,infile,start_cut=0,end_cut=0):
         if key_end in line:
           y_read = 0
           break
-        else: 
+        else:
           grab_list.append(line.strip())
 
   if end_cut == 0:
@@ -83,7 +83,7 @@ def moSplit(q_mos):
     return mo_list
 
 def ChargeGrab(key_end, infile):
-    raw_charge = KeyGrab(key_end, infile, 3, 0) 
+    raw_charge = KeyGrab(key_end, infile, 3, 0)
     atom_list, charge_list, spin_list = [], [], []
     for line in raw_charge:
         spline = line.split()
@@ -107,25 +107,36 @@ def coordGrab(in_file):
 
 def optCoordGrab(in_file,out_file=None):
   key_start = "Standard Nuclear Orientation (Angstroms)"
-  key_end = "--------------"
+  key_end = "Nuclear Repulsion Energy"
   if out_file != None: f_out = open(out_file,'w')
 
-  y_read = False
+  y_read, grab_list = False, []
   with open(in_file,'r') as f:
     for line in f:
+      # copies .xyz info to grab_list; first and last two list items will not represent atoms
+      if y_read == True:
+        grab_list.append(line.strip())
       if key_start in line:
         y_read = True
         grab_list = []                    #dump current grab_list
       elif key_end in line:
         y_read = False
-        if out_file != None:
-          for item in grab_list:
-            f_out.write('%s\n' % item)
-      elif y_read == True:
-        grab_list.append(line.strip())
-  raw_coord = grab_list[2:]
+
+  grab_list_cleaned = []
+  for item in grab_list: # remove list items that do not represent atoms
+    try:
+      int(item.strip().split(' ')[0])
+      grab_list_cleaned.append(item)
+    except:
+      continue
+
+  if out_file != None:
+    f_out.write(str(len(grab_list_cleaned)) + '\n\n')
+    for item in grab_list_cleaned:
+      f_out.write('%s\n' %' '.join(item.split()[1:]))
+
   xyz = []
-  for item in raw_coord:
+  for item in grab_list_cleaned:
     xyz.append(' '.join(item.split()[1:]))
   return xyz
 
@@ -139,7 +150,7 @@ def renameFiles(dir, pattern, ext_pattern):
     print(dir)
     for pathAndFilename in glob.iglob(os.path.join(dir, pattern)):
         title, ext = os.path.splitext(os.path.basename(pathAndFilename))
-        os.rename(pathAndFilename, 
+        os.rename(pathAndFilename,
                   os.path.join(dir, title + ext_pattern))
 
 def SP(fname,coord,charge,mult,method,basis):
@@ -171,20 +182,3 @@ def opt2Sp(in_dir):
   SP(job2,coord,an_charge,2,functional,basis)
 
 #opt2Sp('042715/q_out')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
