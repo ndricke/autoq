4e90f230fb758ce7751b6420e9633cf7b8f2a22f
ndricke <nathan.d.ricke@gmail.com>
2019-02-15 17:37:17 -0500

    Need to re-order H's to the end for all molecules prior to analysis

--- FormO2Df.py
+++ FormO2Df.py
@@ -6,6 +6,7 @@ import pickle
 import subprocess
 from collections import OrderedDict
 import argparse
+import re
 
 import Qdata
 import find_O2, pickle_qout
@@ -13,6 +14,34 @@ import find_O2, pickle_qout
 # check the file name format if this doesn't work
 def catalyst_name(filename): # returns name of bare catalyst given catalystO2 .out file name
     return filename[0:filename[0:filename.find("_")].rfind("O2")]
+
+# to match files of same catalyst with different bound species
+# run name_conv.py first if necessary
+def parse_filename(filename):
+    print(filename)
+    fn_fragments = filename.split('-') #split catalyst type from rest of catalyst info
+    cat_type = fn_fragments[0] #for example, tetrid
+    func_num = re.findall(r'\d+', fn_fragments[1])[0] #find all digits after func, ie 37 in func37
+    print(func_num)
+    if len(fn_fragments) == 3:
+        active_site = re.findall(r'\d+', fn_fragments[2])[0]
+    else:
+        active_site = None
+    return [cat_type, func_num, active_site] #strings
+
+# at this point it probably doesn't matter but it might be more robust to make
+    # parse_filename() find bound species type and then match active site only
+    # if fn1 or fn2 is bare
+def same_catalyst(fn1, fn2, match_active_site):
+    fn1_parsed, fn2_parsed = parse_filename(fn1), parse_filename(fn2)
+    if match_active_site:
+        print(fn1_parsed, fn2_parsed)
+        if fn1_parsed == fn2_parsed:
+            return True
+    else:
+        if fn1_parsed[0] == fn2_parsed[0] and fn1_parsed[1] == fn2_parsed[1]:
+            return True
+    return False
 
 def GenLoadCat(cat_dir, save=False):
     if cat_dir == None:
@@ -33,7 +62,7 @@ def GenLoadCatO2(catO2_dir, save=False):
 ## What needs to happen is, for an entry in the catO2_df, find the corresponding bare catalysts qdata
 class MatchO2(object):
 
-    def __init__(self, in_O2_df, cat_qdata, cation_qdata, OOH_qdata, OH_qdata, O_qdata, CO_qdata, CN_qdata):
+    def __init__(self, in_O2_df, cat_qdata, cation_qdata, OOH_qdata, OH_qdata, O_qdata, CO_qdata) : #, CN_qdata):
 
         energy, cat_fn, AS_CHELPG = "Catalyst_Energy", "Catalyst_File_Name", "Catalyst_Active_Site_CHELPG"
         c1_energy, c1_cat_fn, c1_AS_CHELPG = "Catalyst_c1_Energy", "Catalyst_c1_File_Name", "Catalyst_c1_Active_Site_CHELPG"
@@ -41,7 +70,7 @@ class MatchO2(object):
         O_energy, O_cat_fn, O_AS_CHELPG = "CatalystO_Energy", "CatalystO_File_Name", "CatalystO_Active_Site_CHELPG"
         OH_energy, OH_cat_fn, OH_AS_CHELPG = "CatalystOH_Energy", "CatalystOH_File_Name", "CatalystOH_Active_Site_CHELPG"
         CO_energy, CO_cat_fn, CO_AS_CHELPG = "CatalystCO_Energy", "CatalystCO_File_Name", "CatalystCO_Active_Site_CHELPG"
-        CN_energy, CN_cat_fn, CN_AS_CHELPG = "CatalystCN_Energy", "CatalystCN_File_Name", "CatalystCN_Active_Site_CHELPG"
+        #CN_energy, CN_cat_fn, CN_AS_CHELPG = "CatalystCN_Energy", "CatalystCN_File_Name", "CatalystCN_Active_Site_CHELPG"
 
         self.catO2_df = in_O2_df.copy()
         self.catO2_df = self.catO2_df.assign(Catalyst_File_Name=None)
@@ -68,27 +97,25 @@ class MatchO2(object):
         self.catO2_df = self.catO2_df.assign(CatalystCO_Energy=None)
         self.catO2_df = self.catO2_df.assign(CatalystCO_Active_Site_CHELPG=None)
 
-        self.catO2_df = self.catO2_df.assign(CatalystCN_File_Name=None)
-        self.catO2_df = self.catO2_df.assign(CatalystCN_Energy=None)
-        self.catO2_df = self.catO2_df.assign(CatalystCN_Active_Site_CHELPG=None)
+        #self.catO2_df = self.catO2_df.assign(CatalystCN_File_Name=None)
+        #self.catO2_df = self.catO2_df.assign(CatalystCN_Energy=None)
+        #self.catO2_df = self.catO2_df.assign(CatalystCN_Active_Site_CHELPG=None)
 
-        self.FillMatchedO2(cat_qdata, energy, cat_fn, AS_CHELPG)
-        self.FillMatchedO2(cation_qdata, c1_energy, c1_cat_fn, c1_AS_CHELPG)
-        self.FillMatchedO2(OOH_qdata, OOH_energy, OOH_cat_fn, OOH_AS_CHELPG)
-        self.FillMatchedO2(O_qdata, O_energy, O_cat_fn, O_AS_CHELPG)
-        self.FillMatchedO2(OH_qdata, OH_energy, OH_cat_fn, OH_AS_CHELPG)
-        self.FillMatchedO2(CO_qdata, CO_energy, CO_cat_fn, CO_AS_CHELPG)
-        self.FillMatchedO2(CN_qdata, CN_energy, CN_cat_fn, CN_AS_CHELPG)
+        self.FillMatchedO2(cat_qdata, energy, cat_fn, AS_CHELPG, False)
+        self.FillMatchedO2(cation_qdata, c1_energy, c1_cat_fn, c1_AS_CHELPG, False)
+        self.FillMatchedO2(OOH_qdata, OOH_energy, OOH_cat_fn, OOH_AS_CHELPG, True)
+        self.FillMatchedO2(O_qdata, O_energy, O_cat_fn, O_AS_CHELPG, True)
+        self.FillMatchedO2(OH_qdata, OH_energy, OH_cat_fn, OH_AS_CHELPG, True)
+        self.FillMatchedO2(CO_qdata, CO_energy, CO_cat_fn, CO_AS_CHELPG, True)
+        #self.FillMatchedO2(CN_qdata, CN_energy, CN_cat_fn, CN_AS_CHELPG, True)
 
-    def FillMatchedO2(self, qdatas, energy, cat_fn, AS_CHELPG):
+    def FillMatchedO2(self, qdatas, energy, cat_fn, AS_CHELPG, not_bare):
         if qdatas == None:
             return None # the DF columns will exist but be empty
         for i, entry in enumerate(self.catO2_df["CatalystO2_File_Name"]):
-            #catO2_entry_name = catalyst_name(entry)
-            filled = False # workaround for nonstandard file names
             for qdata in qdatas:
-                #if qdata.filename.split('_')[0] == catO2_entry_name:
-                if qdata.filename.split('_')[1] == entry.split('_')[1] and qdata.filename[0:5] == entry[0:5]:
+                if same_catalyst(entry, qdata.filename, not_bare):
+                    print("I found a match: ", qdata.filename)
                     self.catO2_df.at[i, energy] = qdata.E
                     self.catO2_df.at[i, cat_fn] = qdata.filename
                     try:
@@ -97,21 +124,7 @@ class MatchO2(object):
                         self.catO2_df.at[i,AS_CHELPG] = chelpgs[active_site_index - 1]
                     except:
                         print("Could not get CHELPG charges for " + qdata.filename)
-                    filled = True
                     break # assumes cat_dir does not contain multiple files for the same catalyst
-            if not filled: # be careful with this
-                for qdata in qdatas:
-                    if qdata.filename.split('_')[1] == entry.split('_')[1][0:entry.split('_')[1].find('-')] and qdata.filename[0:5] == entry[0:5]:
-                        self.catO2_df.at[i, energy] = qdata.E
-                        self.catO2_df.at[i, cat_fn] = qdata.filename
-                        try:
-                            chelpgs = [float(item) for item in qdata.chelpg]
-                            active_site_index = self.catO2_df.iloc[i]['Active_Site']
-                            self.catO2_df.at[i,AS_CHELPG] = chelpgs[active_site_index - 1]
-                        except:
-                            print("Could not get CHELPG charges for " + qdata.filename)
-                        filled = True
-                        break # assumes cat_dir does not contain multiple files for the same catalyst
 
 
 if __name__ == "__main__":
@@ -132,7 +145,7 @@ if __name__ == "__main__":
     if (os.path.isfile(args.bare+'.p') and os.path.isfile(args.cation+'.p')
         and os.path.isfile(args.O2+'_df.p') and os.path.isfile(args.OOH+'.p')
         and os.path.isfile(args.OH+'.p') and os.path.isfile(args.O+'.p')
-        and os.path.isfile(args.CO+'.p') and os.path.isfile(args.CN+'.p')):
+        and os.path.isfile(args.CO+'.p')): #and os.path.isfile(args.CN+'.p')):
 
         cat_qdata_list = pickle.load(open(args.bare + ".p", "rb"))
         cation_qdata_list = pickle.load(open(args.cation + ".p", "rb"))
@@ -141,7 +154,10 @@ if __name__ == "__main__":
         OH_qdata_list = pickle.load(open(args.OH + ".p", "rb"))
         O_qdata_list = pickle.load(open(args.O + ".p", "rb"))
         CO_qdata_list = pickle.load(open(args.CO + ".p", "rb"))
-        CN_qdata_list = pickle.load(open(args.CN + ".p", "rb"))
+        #CN_qdata_list = pickle.load(open(args.CN + ".p", "rb"))
+
+        for in_list in [cat_qdata_list, cation_qdata_list, OOH_qdata_list, OH_qdata_list, CO_qdata_list]:
+            print(len(in_list))
     else:
         cat_qdata_list = GenLoadCat(args.bare, save=args.save)
         cation_qdata_list = GenLoadCat(args.cation, save=args.save)
@@ -153,7 +169,7 @@ if __name__ == "__main__":
         CN_qdata_list = GenLoadCat(args.CN, save=args.save)
 
     match = MatchO2(O2_df, cat_qdata_list, cation_qdata_list,
-        OOH_qdata_list, OH_qdata_list, O_qdata_list, CO_qdata_list, CN_qdata_list)
+        OOH_qdata_list, OH_qdata_list, O_qdata_list, CO_qdata_list) #, CN_qdata_list)
     O2_matched_df = match.catO2_df
     print(O2_matched_df)
 
