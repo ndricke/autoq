fe1a0e5652df30cbf67d1a9e59cfb4f590ec2f4f
Kunal Lodaya <kloday88@gmail.com>
2019-06-14 10:26:23 -0400

    Update regression.py

--- regression.py
+++ regression.py
@@ -13,6 +13,7 @@ from rdkit import Chem
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn import svm
 from math import sqrt
 
 
@@ -38,34 +39,39 @@ def linearRegression(cat, O2, plusone, mol, f1, f2, f3, f4, f5, f6, f7, f8, f9, 
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-    model = linear_model.LinearRegression()
-
+    lin_model = linear_model.LinearRegression()
+    svr = svm.SVR(kernel = 'rbf', gamma = 'auto')
     # Train the model using the training sets
-    model.fit(X_train, y_train)
-
+    lin_model.fit(X_train, y_train)
+    svr.fit(X_train, y_train)
     # Make predictions using the testing set
-    y_pred = model.predict(X_test)
-
+    y_pred_lin = lin_model.predict(X_test)
+    y_pred_svr = svr.predict(X_test)
     # The coefficients
-    print('Coefficients: \n', model.coef_)
+    print('Coefficients: \n', lin_model.coef_)
     # The mean squared error
-    print("Root mean squared error: %.2f"
-          % sqrt(mean_squared_error(y_test, y_pred)))
+    print("Root mean squared error, linear: %.2f"
+          % sqrt(mean_squared_error(y_test, y_pred_lin)))
     # Explained variance score: 1 is perfect prediction
-    print('Variance score: %.2f' % r2_score(y_test, y_pred))
-
+    print('Variance score: %.2f' % r2_score(y_test, y_pred_lin))
+    print("Root mean squared error, svr: %.2f"
+          % sqrt(mean_squared_error(y_test, y_pred_svr)))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score: %.2f' % r2_score(y_test, y_pred_svr))
     # Plot outputs
     #print(X_test, y_test)
     if len(cols)==1:
         plt.scatter(X, y,  color='black')
-        plt.plot(X_test, y_pred, color='blue', linewidth=3)
+        plt.plot(X_test, y_pred_lin, color='blue', linewidth=3)
+        plt.plot(X_test, y_pred_svr, color = 'red', linewidth = 3)
 
         plt.xticks(())
         plt.yticks(())
 
         plt.show()
     else:
-        plt.scatter(y_pred, y_test, color='blue')
+        plt.scatter(y_pred_lin, y_test, color='blue')
+        plt.scatter(y_pred_svr, y_test, color = 'red')
 
         plt.xticks(())
         plt.yticks(())
